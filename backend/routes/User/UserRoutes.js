72338186e0774aa6ce672adc84cfd3081b03f994
Samikshagurav <guravsamiksha17@gmail.com>
2022-05-05 09:25:10 +0530

    first commit

---
+++ backend/routes/User/UserRoutes.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const {Register,GetUser, getAll }=require("../../Controller/User/UserController")
+const router = express.Router()
+
+router.post('/register',Register)
+router.get('/getall',getAll)
+router.post('/getuserdata',GetUser)
+
+
+
+
+module.exports=router;
